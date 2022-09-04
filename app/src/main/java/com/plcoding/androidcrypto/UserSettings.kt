a82ea62a4fe23967cd373c629efb457d8c8fbff9
Philipp Lackner <mail@pl-coding.com>
2022-09-04 22:27:42 +0200

    Initial state

--- app/src/main/java/com/plcoding/androidcrypto/UserSettings.kt
+++
@@ -1,9 +0,0 @@
-package com.plcoding.androidcrypto
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class UserSettings(
-    val username: String? = null,
-    val password: String? = null
-)
